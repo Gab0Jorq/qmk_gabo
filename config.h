c3db5c667ad6eccd3511e1df25aee3641dbc8143
Gabriel <gabozx327@gmail.com>
2021-06-12 16:56:32 -0400

    “corne”

---
+++ config.h
@@ -0,0 +1,55 @@
+
+#include "config_common.h"
+#pragma once
+//home rows
+// Configure the global tapping term (default: 200ms)
+#define TAPPING_TERM 200
+
+// Prevent normal rollover on alphas from accidentally triggering mods.
+#define IGNORE_MOD_TAP_INTERRUPT
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define TAPPING_FORCE_HOLD
+
+// Apply the modifier on keys that are tapped during a short hold of a modtap
+#define PERMISSIVE_HOLD
+
+//RGB MODIFICACION POR MI
+#define RGBLIGHT_LAYERS
+
+
+
+
+//#define USE_MATRIX_I2C
+
+/* Select hand configuration */
+
+#define MASTER_LEFT
+// #define MASTER_RIGHT
+// #define EE_HANDS
+
+#define USE_SERIAL_PD2
+
+
+#ifdef RGBLIGHT_ENABLE
+    #undef RGBLED_NUM
+    #define RGBLIGHT_ANIMATIONS
+    #define RGBLED_NUM 27
+    #define RGBLIGHT_LIMIT_VAL 120
+    #define RGBLIGHT_HUE_STEP 10
+    #define RGBLIGHT_SAT_STEP 17
+    #define RGBLIGHT_VAL_STEP 17
+#endif
+
+
+//animacion
+#ifdef OLED_DRIVER_ENABLE
+  //#define OLED_DISPLAY_128X64
+  #define OLED_TIMEOUT 300000
+#endif
+
+#define SPLIT_USB_DETECT
+
+#define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
+
+	#define NO_ACTION_ONESHOT
---
+++ rules.mk
@@ -0,0 +1,39 @@
+DEFAULT_FOLDER = crkbd/rev1
+
+#Agregado x mi
+MCU = atmega32u4
+
+# Bootloader selection
+#   Teensy       halfkay
+#   Pro Micro    caterina
+#   Atmel DFU    atmel-dfu
+#   LUFA DFU     lufa-dfu
+#   QMK DFU      qmk-dfu
+#   ATmega32A    bootloadHID
+#   ATmega328P   USBasp
+#BOOTLOADER = atmel-dfu
+BOOTLOADER = caterina
+
+BOOTMAGIC_ENABLE = no  # Virtual DIP switch configuration(+1000)
+MOUSEKEY_ENABLE  = yes # Mouse keys(+4700)
+EXTRAKEY_ENABLE  = yes  # Audio control and System control(+450)
+CONSOLE_ENABLE   = no  # Console for debug(+400)
+COMMAND_ENABLE   = no  # Commands for debug and configuration
+NKRO_ENABLE      = yes # Nkey Rollover
+BACKLIGHT_ENABLE = no  # Enable keyboard backlight functionality
+MIDI_ENABLE      = no  # MIDI support (+2400 to 4200, depending on config)
+AUDIO_ENABLE     = no  # Audio output on port C6
+UNICODE_ENABLE   = no  # Unicode
+BLUETOOTH_ENABLE = no  # Enable Bluetooth with the Adafruit EZ-Key HID
+RGBLIGHT_ENABLE  = yes  # Enable WS2812 RGB underlight.  Do not enable this with audio at the same time.
+SLEEP_LED_ENABLE = no  # Breathing sleep LED during USB suspend
+STENO_ENABLE     = no  # enable TX Bolt protocol, requires VIRTSER and may not work with mouse keys
+TAP_DANCE_ENABLE = no # enable double tap
+# VIRTSER_ENABLE = yes # required for TX Bolt
+
+
+
+#prueba dibujo
+WPM_ENABLE = yes
+OLED_DRIVER_ENABLE = yes
+LTO_ENABLE = yes    #HACER MAS LIVIANO EL ARCHIVO
